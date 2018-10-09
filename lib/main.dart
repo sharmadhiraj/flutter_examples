7c3a0e0b033b75afe67f7a5c4b1dcac00a7cdabb
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2018-10-09 11:15:01 +0545

    Flutter package & Dart upgrade.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,5 @@ import 'package:flutter/material.dart';
 import 'pages/list.dart';
 
 void main() {
-  // ignore: deprecated_member_use
-  MaterialPageRoute.debugEnableFadingRoutes = true;
   runApp(MaterialApp(home: ListScreen()));
 }
